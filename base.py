6c5cb678d67e7ff3d2ffaedc5bb6c28f8eb64508
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2020-02-29 07:45:02 +0100

    base: use litex-boards colorlight_5a_75b platform instead of custom. Serial is now an extension

--- base.py
+++ base.py
@@ -4,8 +4,8 @@ from migen import *
 
 from migen.genlib.io import CRG
 
-from litex.build.generic_platform import *
-from litex.build.lattice import LatticePlatform
+from litex.build.generic_platform import IOStandard, Subsignal, Pins
+from litex_boards.platforms import colorlight_5a_75b
 
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
@@ -14,41 +14,22 @@ from ios import Led
 
 # IOs ----------------------------------------------------------------------------------------------
 
-_io = [
-    # clock
-    ("clk25", 0, Pins("P6"), IOStandard("LVCMOS33")),
-
-    # led (n)
-    ("user_led", 0, Pins("P11"), IOStandard("LVCMOS33")),
-    
-    # btn (n)
-    ("cpu_reset", 0, Pins("M13"), IOStandard("LVCMOS33")),
-    
+_serial = [
     ("serial", 0,
         Subsignal("tx", Pins("F3")), # J1.1
         Subsignal("rx", Pins("F1")), # J1.2
-        IOStandard("LVCMOS33")),
-
+        IOStandard("LVCMOS33")
+    ),
 ]
 
-# Platform -----------------------------------------------------------------------------------------
+# BaseSoC -----------------------------------------------------------------------------------------
 
-class Platform(LatticePlatform):
-    default_clk_name   = "clk25"
-    default_clk_period = 1e9/25e6
-
-    def __init__(self):
-        LatticePlatform.__init__(self, "LFE5U-25F-6BG256C", _io, toolchain="trellis")
-
-# Design -------------------------------------------------------------------------------------------
-
-# Create our platform (fpga interface)
-platform = Platform()
-
-# Create our soc (fpga description)
 class BaseSoC(SoCCore):
-    def __init__(self, platform):
+    def __init__(self, revision):
+        platform = colorlight_5a_75b.Platform(revision)
         sys_clk_freq = int(25e6)
+
+        platform.add_extension(_serial)
 
         # SoC with CPU
         SoCCore.__init__(self, platform,
@@ -59,16 +40,17 @@ class BaseSoC(SoCCore):
             integrated_main_ram_size = 0x4000)
 
         # Clock Reset Generation
-        self.submodules.crg = CRG(platform.request("clk25"), ~platform.request("cpu_reset"))
+        self.submodules.crg = CRG(platform.request("clk25"), ~platform.request("user_btn_n"))
 
         # Led
-        user_leds = Cat(*[platform.request("user_led", i) for i in range(1)])
+        user_leds = Cat(*[platform.request("user_led_n", i) for i in range(1)])
         self.submodules.leds = Led(user_leds)
         self.add_csr("leds")
 
-soc = BaseSoC(platform)
 
 # Build --------------------------------------------------------------------------------------------
+
+soc = BaseSoC(revision="7.0")
 
 builder = Builder(soc, output_dir="build", csr_csv="test/csr.csv")
 builder.build()
